0479b3125d1433b39f7cd5aa59d6ba8fc68b4547
Nicolai Wiskemann <nicolai.wiskemann@gmail.com>
2017-02-15 10:54:36 +0000

    rails new

---
+++ db/migrate/20170215104500_create_restaurants.rb
@@ -0,0 +1,15 @@
+class CreateRestaurants < ActiveRecord::Migration[5.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :string
+      t.string :address
+      t.string :string
+      t.string :phone_number
+      t.string :integer
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
